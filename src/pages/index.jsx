b8978912f7203d2509013f5a7b420df192192de3
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-04-07 21:26:08 -0300

    Navegação entra páginas

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,14 @@
+import Link from 'next/link'
+
 export default function Inicio() {
     //"<h1>Título</h1>"
     // JSX - permite escrever trechos HTML no javascript e quando renderizado no browser transpila para HTML 
     //return <h1>Título</h1>
     return (
         <div>
-            <h1>Fundamentos de Next.js & React</h1>
-            <h2>Vamos estudar esse framewrok</h2>
+           <Link href="/estiloso">
+               Estiloso
+           </Link>
         </div>
     )
 }
