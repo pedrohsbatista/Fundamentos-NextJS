d0aa9d4d6da3e1b334aecfe75f8caaa137e63cda
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-04-08 21:24:56 -0300

    Navegação dinâmica

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,6 +16,7 @@ export default function Inicio() {
            <Navegador destino="/exemplo"   texto="Exemplo"       cor="#9400d3" />
            <Navegador destino="/jsx"       texto="Jsx"           cor="crimson" />
            <Navegador destino="/navegacao" texto="Navegação #01" cor="#2f4f4f" />
+           <Navegador destino="cliente/A/1"  texto="Navegação #02" cor="#cf8803"/>
         </div>
     )
 }
