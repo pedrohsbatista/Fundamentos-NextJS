54bf087799f6a316f99a7cf40f13e70ae4e023a8
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-04-11 20:27:26 -0300

    Integração API

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,11 +13,12 @@ export default function Inicio() {
                 height: '100vh',
             }}>
            <Navegador destino="/estiloso"     texto="Estiloso" />
-           <Navegador destino="/exemplo"      texto="Exemplo"               cor="#9400d3" />
-           <Navegador destino="/jsx"          texto="Jsx"                   cor="crimson" />
-           <Navegador destino="/navegacao"    texto="Navegação #01"         cor="#2f4f4f" />
-           <Navegador destino="/cliente/A/1"  texto="Navegação #02"         cor="#cf8803" />
-           <Navegador destino="/estado"       texto="Componente com Estado" cor="#464646" />
+           <Navegador destino="/exemplo"      texto="Exemplo"                cor="#9400d3" />
+           <Navegador destino="/jsx"          texto="Jsx"                    cor="crimson" />
+           <Navegador destino="/navegacao"    texto="Navegação #01"          cor="#2f4f4f" />
+           <Navegador destino="/cliente/A/1"  texto="Navegação #02"          cor="#cf8803" />
+           <Navegador destino="/estado"       texto="Componente com Estado"  cor="#464646" />
+           <Navegador destino="/integracao"   texto="Integração com API #01" cor="#0011ff" />
         </div>
     )
 }
---
+++ src/pages/integracao1.jsx
@@ -0,0 +1,34 @@
+import Layout from '../components/Layout'
+import { useState } from 'react'
+
+export default function Integracao() {
+
+    const [codigo, setCodigo] = useState(1)
+    const [cliente, setCliente] = useState({})    
+    
+    // function getCliente() {
+    //     fetch(`http://localhost:3000/api/clientes/${codigo}`)
+    //     .then(resp => resp.json())
+    //     .then(dados => setCliente(dados))
+    // }    
+
+    async function getCliente() {
+        const resp = await fetch(`http://localhost:3000/api/clientes/${codigo}`)
+        const dados = await resp.json()
+        setCliente(dados)
+    }   
+
+    return(
+        <Layout>
+            <div>
+                <input type="number" value={codigo} onChange={e => setCodigo(e.target.value)}/>
+                <button onClick={getCliente}>Obter Cliente</button>
+            </div>            
+            <ul>
+                <li>Código:  {cliente.id} </li>
+                <li>Nome:    {cliente.nome}   </li>
+                <li>Idade:   {cliente.idade}  </li>
+            </ul>
+        </Layout>
+    )
+}
